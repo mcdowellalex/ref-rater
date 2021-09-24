2c4aca890dc51e15cab9d75b66f586a451ecd572
mcdowellalex <alexmichaelmcdowell@gmail.com>
2021-09-24 10:33:58 -0400

    get data from API

--- elenasportTest.py
+++ elenasportTest.py
@@ -1,10 +1,24 @@
 import requests
 
-url=''
+# used for asking for data
+access_token=''
+
+# used for asking for access token
+accessTokenURL=''
 authorization=''
 content_type='application/x-www-form-urlencoded'
 grant_type='client_credentials'
 
-response = requests.post(url, headers={'Authorization':authorization, 'Content-Type':content_type}, data={'grant_type':grant_type})
+# asking for access token
+response = requests.post(accessTokenURL, headers={'Authorization':authorization, 'Content-Type':content_type}, data={'grant_type':grant_type})
+data = response.json()
+access_token = data['access_token']
+# print(access_token)
+
+
+# get data
+getRequestURL = 'https://football.elenasport.io/v2/countries'
+getAuthorization = 'Bearer ' + access_token
+response = requests.get(getRequestURL, headers={'Authorization':getAuthorization})
 
 print(response.json())
