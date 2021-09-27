ed39a478fc174d3cfb40fae3a99689b18a2ccf1a
mcdowellalex <alexmichaelmcdowell@gmail.com>
2021-09-27 18:17:19 -0400

    added functionality to request from auth.json

--- elenasportTest.py
+++ elenasportTest.py
@@ -1,24 +1,31 @@
 import requests
+import json
 
-# used for asking for data
-access_token=''
+# ----------------- setup ------------------
+# get auth data from auth file
+with open('auth.json', 'r') as myfile:
+    data=myfile.read()
+    dataJSON = json.loads(data)
+    authJSON = dataJSON['elenasport']
 
 # used for asking for access token
-accessTokenURL=''
-authorization=''
-content_type='application/x-www-form-urlencoded'
-grant_type='client_credentials'
+accessTokenURL=authJSON['endpoint']
+authorization=authJSON['authorization']
+content_type=authJSON['content-type']
+grant_type=authJSON['grant-type']
 
-# asking for access token
+# ask for access token
 response = requests.post(accessTokenURL, headers={'Authorization':authorization, 'Content-Type':content_type}, data={'grant_type':grant_type})
 data = response.json()
-access_token = data['access_token']
+access_token = data['access_token'] # used for API requests
 # print(access_token)
 
 
-# get data
+# ----------------- HTTPS requests ------------------
 getRequestURL = 'https://football.elenasport.io/v2/countries'
 getAuthorization = 'Bearer ' + access_token
 response = requests.get(getRequestURL, headers={'Authorization':getAuthorization})
 
 print(response.json())
+
+
